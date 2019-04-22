a17a07473c3624b1da44aebf3e4b75b82b879864
Ambika <ambikula@gmail.com>
2019-04-21 17:25:37 -0700

    active listitem color changing while scrolling

--- client/src/components/Header-Footer/Header.js
+++ client/src/components/Header-Footer/Header.js
@@ -4,26 +4,42 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import SideDrawer from '../SideDrawer/SideDrawer';
-
-
+import {scroller} from 'react-scroll';
 
 class Header extends Component {
     state = {
         showSidebar : false,
-        showHeader:false
+        showHeader:false,
+        active:'home'
     }
     componentDidMount(){
         window.addEventListener('scroll', this.handleScroll)
     }
-    
+    scrollTOElement = (element) =>{        
+        scroller.scrollTo(element, {
+            duration:1500,
+            delay:100,
+            smooth:true
+        })
+        this.setState({active:element})
+    }
     ToggleButton = (value) => {
         this.setState({showSidebar:value});
     }
     handleScroll = () => {
-        if(window.scrollY>0){
-            this.setState({showHeader:true});
+        if(window.scrollY>0 && window.scrollY<=300){
+            this.setState({showHeader:true, active:"home"});
+        }
+        else if(window.scrollY>400 && window.scrollY<1200){
+            this.setState({showHeader:true, active:"about"});
+        }
+        else if(window.scrollY>=1200 && window.scrollY<1600){
+            this.setState({showHeader:true, active:"portfolio"});
+        }
+        else if(window.scrollY>1600){
+            this.setState({showHeader:true, active:"contact"})
         }else{
-            this.setState({showHeader:false})
+            this.setState({showHeader:false, actve:"home"})
         }
     }
     render() {    
@@ -36,7 +52,7 @@ class Header extends Component {
                             <MenuIcon />
                         </IconButton> : null}
                     </Toolbar>
-                    <SideDrawer open = {this.state.showSidebar} onclose = {(value)=> this.ToggleButton(value)}/>
+                    <SideDrawer open = {this.state.showSidebar} active = {this.state.active} scrolllTOElement = {(element)=>this.scrollTOElement(element)} onclose = {(value)=> this.ToggleButton(value)}/>
                     
                 </AppBar>
             </div>
--- client/src/components/SideDrawer/SideDrawer.js
+++ client/src/components/SideDrawer/SideDrawer.js
@@ -3,44 +3,44 @@ import Drawer from '@material-ui/core/Drawer';
 import { withStyles } from "@material-ui/core/styles";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import {scroller} from 'react-scroll'
+
 const styles = {    
     paper: {
         background: 'rgba(20, 41, 51,0.3)',
         color:'#fff',
         padding:'10px'
     },
-    
+    active: {
+        color: "#43DDE0"
+      }
   };
-const SideDrawer = (props) => {
-    const { classes } = props;
-    const scrolllTOElement = (element) =>{
-        scroller.scrollTo(element, {
-            duration:1500,
-            delay:100,
-            smooth:true
-        })
-    }
+class SideDrawer extends React.Component{
+       
+
+    render(){
+    const { classes } = this.props;
+    
     return (
         <div>
-            <Drawer classes={{paper:classes.paper}} open={props.open} onClose={()=> props.onclose(false)}>
+            <Drawer classes={{paper:classes.paper}} open={this.props.open} onClose={()=> this.props.onclose(false)}>
                <List component='nav'>
-                   <ListItem button onClick ={()=>scrolllTOElement('home')}> 
+                   <ListItem button  selected={this.props.active ==='home' ? true : false } classes={{ selected: classes.active }} onClick ={()=>this.props.scrolllTOElement('home')}> 
                        HOME
                    </ListItem>
-                   <ListItem button onClick ={()=>scrolllTOElement('about')}>
+                   <ListItem button selected={this.props.active ==='about' ? true : false } classes={{ selected: classes.active }} onClick ={()=>this.props.scrolllTOElement('about')}>
                        ABOUT
                    </ListItem>
-                   <ListItem button onClick ={()=>scrolllTOElement('portfolio')}>
+                   <ListItem button selected={this.props.active ==='portfolio' ? true : false } classes={{ selected: classes.active }} onClick ={()=>this.props.scrolllTOElement('portfolio')}>
                       PORTFOLIO
                    </ListItem>
-                   <ListItem button onClick ={()=> scrolllTOElement('contact')}>
+                   <ListItem button selected={this.props.active ==='contact' ? true : false } classes={{ selected: classes.active }} onClick ={()=>this.props.scrolllTOElement('contact')}>
                        CONTACT
                    </ListItem>
                </List>
             </Drawer>
         </div>
     );
+}
 }
 
 export default withStyles(styles)(SideDrawer);
